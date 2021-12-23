c196f006283c110d9d4661d42b352a9cebea2431
Hooshyar <hooseyr@gmail.com>
2021-12-23 18:07:16 +0300

    Theming added

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:wecode_2021/src/home_screen/home_screen_view.dart';
 import 'package:wecode_2021/src/login_screen/login_screen_view.dart';
 import 'package:wecode_2021/src/privacy_policy/privacy_policy_screen.dart';
@@ -6,10 +7,21 @@ import 'package:wecode_2021/src/student_screen/student_screen_view.dart';
 
 class AppView extends StatelessWidget {
   const AppView({Key? key}) : super(key: key);
-
+  final String selectedLang = 'ar';
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+          primaryColor: Colors.red,
+          textTheme: TextTheme(
+              bodyText1: TextStyle(
+            fontFamily: selectedLang == 'ar'
+                ? GoogleFonts.robotoCondensed().fontFamily
+                : GoogleFonts.roboto().fontFamily,
+          )),
+          appBarTheme: AppBarTheme(
+            backgroundColor: Colors.red,
+          )),
       initialRoute: '/',
       routes: {
         '/': (context) => HomeScreenView(),
