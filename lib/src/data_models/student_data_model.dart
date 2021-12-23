35c87bed52fcee977605ed987023ad794596114d
Hooshyar <hooseyr@gmail.com>
2021-12-23 15:42:01 +0300

    Created the Student data model.

---
+++ lib/src/data_models/student_data_model.dart
@@ -0,0 +1,20 @@
+class Student {
+  String? name;
+  String? email;
+  String? github;
+  String? stackOverflow;
+  int? points;
+  int? thumbsUps;
+
+  Student(
+    this.name,
+    this.email,
+    this.github,
+    this.stackOverflow,
+    this.points,
+    this.thumbsUps,
+
+    //todo: convert a json to this object
+    //todo: convert this object to a json
+  );
+}
